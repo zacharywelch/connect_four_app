0a1349e09e26797ffee459208bdf3c2e90dd1e41
zwelchcb <Zachary.Welch@careerbuilder.com>
2016-12-11 16:31:37 -0500

    add board partial

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,10 @@
 module GamesHelper
+  def cell_for(game, row, column)
+    move = game.moves.find { |move| move.row == row && move.column == column }
+    if move.present?
+      move.value
+    else
+      '-'
+    end
+  end
 end
