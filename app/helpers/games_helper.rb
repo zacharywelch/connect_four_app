190d4036800bde188a5fa40efc4e1d5bb7470b81
zwelchcb <Zachary.Welch@careerbuilder.com>
2016-12-11 17:32:07 -0500

    add disc styles to board

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -2,9 +2,15 @@ module GamesHelper
   def cell_for(game, row, column)
     move = game.moves.find { |move| move.row == row && move.column == column }
     if move.present?
-      move.value
+      content_tag :div, nil, class: move.value == 'x' ? 'disc bg-primary' : 'disc bg-danger'
+    elsif game.over?
+      content_tag :div, nil, class: 'disc'
     else
-      '-'
+      value = @game.turn == @game.player_one ? 'x' : 'o'
+      link_to '', 
+              game_moves_path(@game, move: { column: column, value: value }), 
+              method: :post,
+              class: 'disc'
     end
   end
 end
